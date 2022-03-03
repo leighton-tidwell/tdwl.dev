ef6a5f375707cb4b9f4b93068112021f0a7b94e5
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-03-03 19:29:37 +0000

    Setup of initial home page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
+module.exports = withMDX({ pageExtensions: ["js", "mdx"] });
